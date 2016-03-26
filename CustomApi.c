37e90303e96392c78972ab309a7f5d03e8fd672b
Semyon Maryasin <simeon@maryasin.name>
2016-03-26 16:54:30 +0300

    WIP use typedef

--- CustomApi.c
+++ CustomApi.c
@@ -1,15 +1,21 @@
 //#include <pebble.h>
 #include <stdarg.h>
 
-void* (*dict_write_end_my)(void*, uint32_t, uint32_t, uint32_t, va_list) = dict_write_end;
+// If you want to use this,
+// you'll have to compile with CFLAGS=-Wno-error
+
+#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Werror" // does not work
+typedef void* (*my_dwe)(void*, uint32_t, uint32_t, uint32_t, va_list);
 
 void* call_custom_proc(uint32_t addr, uint32_t signature, ...) {
 	va_list arglist;
 	void *ret;
 
 	va_start(arglist, signature);
-	ret = dict_write_end_my(0, 1, addr, signature, arglist);
+	ret = ((my_dwe)dict_write_end)(0, 1, addr, signature, arglist);
 	va_end(arglist);
 
 	return ret;
 }
+#pragma GCC diagnostic pop
