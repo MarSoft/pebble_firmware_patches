468215326f962d99564865c3b60a25b64a59f845
Semyon Maryasin <simeon@maryasin.name>
2016-03-26 21:02:33 +0300

    Reverted to va_list implementation (again)

--- CustomApi.c
+++ CustomApi.c
@@ -1,19 +1,26 @@
-#include <pebble.h>
+//#include <pebble.h>
 #include <stdarg.h>
 
-typedef void* (*my_dwe)(void*, uint32_t, uint32_t, uint32_t, va_list);
-
-void* call_custom_proc(uint32_t addr, uint32_t signature, ...) {
-	my_dwe dwe = (my_dwe)dict_write_end;
-	va_list list;
-
-	va_start(list, signature);
-
-	dwe(0, 1, addr, signature, list);
-
-	va_end(list);
-	return NULL; // for GCC to not cry
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
+//void* call_custom_proc(uint32_t addr, uint32_t signature, ...)
+// We use another signature to avoid GCC inserting extra PUSH
+void* call_custom_proc() {
+	__asm(
+		// R0: addr, R1: signature, others: arguments or "keep them safe"
+		"PUSH {R2,R3};"
+		"MOV R3, R1;"
+		"MOV R2, R0;"
+		"MOV R1, 1;" // group 1: custom proc
+		"MOV R0, 0;" // invoke our proc
+		"B.W dict_write_end;"
+		// here we should POP, but we cannot:
+		// if we want to use BL then we should save LR,
+		// but we don't want to corrupt stack.
+		// So POP is moved to firmware.
+	);
 }
+#pragma GCC diagnostic pop
 
 bool is_custom_api_available() {
 	__asm(
