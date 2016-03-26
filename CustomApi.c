bc54689da0c865dab5b62cfc9361f948a198f61c
Semyon Maryasin <simeon@maryasin.name>
2016-03-26 18:52:45 +0300

    Rewrote C procs in assembler, w/o va_list

--- CustomApi.c
+++ CustomApi.c
@@ -4,18 +4,28 @@
 // If you want to use this,
 // you'll have to compile with CFLAGS=-Wno-error
 
-#pragma GCC diagnostic push
-//#pragma GCC diagnostic ignored "-Werror" // does not work
-typedef void* (*my_dwe)(void*, uint32_t, uint32_t, uint32_t, va_list);
-
 void* call_custom_proc(uint32_t addr, uint32_t signature, ...) {
-	va_list arglist;
-	void *ret;
-
-	va_start(arglist, signature);
-	ret = ((my_dwe)dict_write_end)(0, 1, addr, signature, arglist);
-	va_end(arglist);
-
-	return ret;
+	__asm(
+		// R0: addr, R1: signature, others: arguments or "keep them safe"
+		"PUSH {R2,R3};"
+		"MOV R3, R1;"
+		"MOV R2, R0;"
+		"MOV R1, 1;" // group 1: custom proc
+		"MOV R0, 0;" // invoke our proc
+		"B.W dict_write_end;"
+		// here we should POP, but we cannot:
+		// if we want to use BL then we should save LR,
+		// but we don't want to corrupt stack.
+		// So POP is moved to firmware.
+	);
+	return NULL; // for GCC to not cry
 }
-#pragma GCC diagnostic pop
+
+bool is_custom_api_available() {
+	__asm(
+		"MOV R0, 0;" // invoke our proc
+		"MOV R1, 0;" // group 0: check availability
+		"B.W dict_write_end;"
+	);
+	return false; // for GCC to be happy
+}
